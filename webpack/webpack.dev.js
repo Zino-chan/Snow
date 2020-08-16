da64afba209ab06d415075616255bfd7ba018b53
Zino-chan <webdevzino@gmail.com>
2020-08-16 20:00:58 +0100

    Added base files and styles

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+    mode: "development",
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist/assets"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Snow',
+            favicon:  "./public/favicon.png",
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+        // new HtmlWebpackPlugin({
+        //     title: 'Snow',
+        //     favicon:  './public/favicon.png',
+        //     template: './src/about.html',
+        //     filename: 'about.html',
+        // }),
+        // new HtmlWebpackPlugin({
+        //     title: 'Snow',
+        //     favicon:  './public/favicon.png',
+        //     template: './src/reservation.html',
+        //     filename: 'reservation.html',
+        // }),
+        // new HtmlWebpackPlugin({
+        //     title: 'Snow',
+        //     favicon:  './public/favicon.png',
+        //     template: './src/services.html',
+        //     filename: 'services.html',
+        // }),
+        // new HtmlWebpackPlugin({
+        //     title: 'Snow',
+        //     favicon:  './public/favicon.png',
+        //     template: './src/menu.html',
+        //     filename: 'menu.html',
+        // }),
+        // new HtmlWebpackPlugin({
+        //     title: 'Snow',
+        //     favicon:  './public/favicon.png',
+        //     template: './src/contact.html',
+        //     filename: 'contact.html',
+        // })
+
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s(a|c)ss$/,
+                use: [
+                    "style-loader",
+                    "css-loader", 
+                    "sass-loader" 
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader", 
+                    "sass-loader" 
+                ],
+            },
+
+        ]
+    }
+});
---
+++ webpack/webpack.prod.js
@@ -0,0 +1,124 @@
+const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    filename: "[name].[contentHash].js",
+    path: path.resolve(__dirname, "../dist/assets")
+  },
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Snow',
+        favicon:  "./public/favicon.png",
+        template: "./src/index.html",
+        filename: '../index.html',
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true
+        }
+      }),
+      // new HtmlWebpackPlugin({
+      //   title: 'Snow',
+      //   favicon:  './public/favicon.png',
+      //   template: "./src/about.html",
+      //   filename: '../about.html',
+      //   minify: {
+      //     removeAttributeQuotes: true,
+      //     collapseWhitespace: true,
+      //     removeComments: true
+      //   }
+      // }),
+      // new HtmlWebpackPlugin({
+      //   title: 'Snow',
+      //   favicon:  './public/favicon.png',
+      //   template: "./src/reservation.html",
+      //   filename: '../reservation.html',
+      //   minify: {
+      //     removeAttributeQuotes: true,
+      //     collapseWhitespace: true,
+      //     removeComments: true
+      //   }
+      // }),
+      // new HtmlWebpackPlugin({
+      //   title: 'Snow',
+      //   favicon:  './public/favicon.png',
+      //   template: "./src/services.html",
+      //   filename: '../services.html',
+      //   minify: {
+      //     removeAttributeQuotes: true,
+      //     collapseWhitespace: true,
+      //     removeComments: true
+      //   }
+      // }),
+      // new HtmlWebpackPlugin({
+      //   title: 'Snow',
+      //   favicon:  './public/favicon.png',
+      //   template: "./src/menu.html",
+      //   filename: '../menu.html',
+      //   minify: {
+      //     removeAttributeQuotes: true,
+      //     collapseWhitespace: true,
+      //     removeComments: true
+      //   }
+      // }),
+      // new HtmlWebpackPlugin({
+      //   title: 'Snow',
+      //   favicon:  './public/favicon.png',
+      //   template: "./src/contact.html",
+      //   filename: '../contact.html',
+      //   minify: {
+      //     removeAttributeQuotes: true,
+      //     collapseWhitespace: true,
+      //     removeComments: true
+      //   }
+      // })
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(
+      { filename: "[name].[contentHash].css" }),
+
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s(a|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: 'assets',
+            },
+          },
+          "css-loader", 
+          "sass-loader" 
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+                {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  publicPath: 'assets',
+                },
+              },
+            'css-loader',
+            'sass-loader'
+        ],
+    },
+    ]
+  }
+});
