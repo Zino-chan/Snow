784d5e5c37add745d08e8b1db0c49e8a23266394
Zino-chan <webdevzino@gmail.com>
2020-08-17 19:14:36 +0100

    add sal.js and splide.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,29 @@
 import '../scss/main.scss';
 import header from '../components/header.html';
 import footer from '../components/footer.html';
-
+import sal from 'sal.js';
+import Splide from '@splidejs/splide';
 
 document.getElementById('header').innerHTML = header;
 
 document.getElementById('footer').innerHTML = footer;
 
-window.addEventListener('scroll', () => {
-    if(scrollY > 80){
-        document.getElementById('header').classList.add('scrolled');
-    }else{
-        document.getElementById('header').classList.remove('scrolled');
-    }
+document.addEventListener('DOMContentLoaded', () => {
+
+    window.addEventListener('scroll', () => {
+        scrollY > 80 ? document.getElementById('header').classList.add('scrolled')
+        : document.getElementById('header').classList.remove('scrolled');
+    })
+
+    sal();
+
+    new Splide( '.splide', {
+        type        : 'loop',
+        perPage     : 1,
+        autoplay    : true,
+        pauseOnHover: true,
+        arrows: false,
+        speed: 300
+    }).mount();
 })
+
--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,1 +1,3 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'sal.js/dist/sal.css';
+import '@splidejs/splide/dist/css/splide.min.css';
