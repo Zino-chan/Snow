1e7ce6eb28b8210139991136c296bac132dbe8e6
Zino-chan <webdevzino@gmail.com>
2020-08-17 17:52:38 +0100

    Finish page

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
 import '../scss/main.scss';
 import header from '../components/header.html';
+import footer from '../components/footer.html';
+
 
 document.getElementById('header').innerHTML = header;
+
+document.getElementById('footer').innerHTML = footer;
+
+window.addEventListener('scroll', () => {
+    if(scrollY > 80){
+        document.getElementById('header').classList.add('scrolled');
+    }else{
+        document.getElementById('header').classList.remove('scrolled');
+    }
+})
