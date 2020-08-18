eeb1e96944ad31cf179d4b19f6e3bbdc03a27001
Zino-chan <webdevzino@gmail.com>
2020-08-18 10:26:22 +0100

    finish preloader

--- src/js/main.js
+++ src/js/main.js
@@ -4,20 +4,23 @@ import footer from '../components/footer.html';
 import sal from 'sal.js';
 import Splide from '@splidejs/splide';
 
-document.getElementById('header').innerHTML = header;
+const selector = (element) => document.querySelector(element);
 
+document.getElementById('header').innerHTML = header;
 document.getElementById('footer').innerHTML = footer;
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    selector('.preloader').style.display = 'none';
 
     window.addEventListener('scroll', () => {
         scrollY > 80 ? document.getElementById('header').classList.add('scrolled')
         : document.getElementById('header').classList.remove('scrolled');
     });
 
-    document.querySelector('.nav-toggler').addEventListener('click', () => {
-        document.querySelector('.navbar-nav').classList.toggle('open');
-        document.querySelector('.nav-toggler').classList.toggle('open');
+    selector('.nav-toggler').addEventListener('click', () => {
+        selector('.navbar-nav').classList.toggle('open');
+        selector('.nav-toggler').classList.toggle('open');
     })
 
     sal();
